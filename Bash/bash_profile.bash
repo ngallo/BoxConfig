74533f9bd8bc6b884c9c1db2ea9c8a91b23e774e
Nicola Gallo <nicolagmt@hotmail.com>
2017-04-01 16:28:41 +0100

    Amended gitconfig in order to support vscode and kdiff3

--- Bash/bash_profile.bash
+++ Bash/bash_profile.bash
@@ -2,8 +2,9 @@
 # ~/.bash_profile: executed by bash for login shells.
 #
 parse_git_branch() {
-     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
-export PS1="\u@\h \[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ "
+export PS1="\u@\h \W\[\033[32m\]\$(parse_git_branch)\[\033[00m\] $ "
 
-source /usr/local/lib/dnx/bin/dnvm.sh
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+eval $(/usr/libexec/path_helper -s)
