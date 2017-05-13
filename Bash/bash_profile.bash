31a4adeb86a6efe020abecb930b3f5759a9f0b63
Nicola Gallo <nicolagmt@hotmail.com>
2017-05-13 21:21:59 +0100

    Amended bash_profile in order to map the # key

--- Bash/bash_profile.bash
+++ Bash/bash_profile.bash
@@ -8,3 +8,5 @@ export PS1="\u@\h \W\[\033[32m\]\$(parse_git_branch)\[\033[00m\] $ "
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 eval $(/usr/libexec/path_helper -s)
+
+bind '"\e3":"#"'
