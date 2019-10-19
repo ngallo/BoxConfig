eca32d3f7f2e487ecf4b3eb8ba2da0e975a4c9c7
Nicola Gallo <nicolagmt@hotmail.com>
2019-10-20 00:00:13 +0200

    Configured VIM to use Plug and fzf

--- nicolagmt-lnx.sh
+++ nicolagmt-lnx.sh
@@ -1,3 +1,4 @@
 sudo apt update
 sudo apt-get install fzf
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 bash install.sh -v nicolagmt-lnx.cfg 
