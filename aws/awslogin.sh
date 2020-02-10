d8688f2cedc5cb689b766b572342af7695481825
Nicola Gallo <nicolagmt@hotmail.com>
2020-02-10 15:14:06 +0100

    Amended AWS login

--- aws/awslogin.sh
+++ aws/awslogin.sh
@@ -5,7 +5,7 @@ BASE64_ENCODED_RESPONSE=$1
 echo 
 rm ~/.aws/credentials
 aws sts assume-role-with-saml --role-arn $AWS_ROLE --principal-arn $AWS_PRINCIPAL --saml-assertion $BASE64_ENCODED_RESPONSE | awk -F:  '
-                BEGIN { RS = "[,{}]" ; print "[default]"}
+                BEGIN { RS = "[,{}]" ; print "[nitro-factory]"}
                 /:/{ gsub(/"/, "", $2) }
                 /AccessKeyId/{ print "aws_access_key_id = " $2 }
                 /SecretAccessKey/{ print "aws_secret_access_key = " $2 }
